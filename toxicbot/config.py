4e13ae0c0e1300738faeacb59708bd7824c9dc68
Vyacheslav Syrovatsky <slasyz@gmail.com>
2021-05-08 23:34:01 +0300

    Move database creds and telegram token back to secrets.

--- toxicbot/config.py
+++ toxicbot/config.py
@@ -1,11 +1,29 @@
 import json
+from typing import Union
+
+
+def load_secret(src: Union[dict, str]) -> str:
+    if isinstance(src, str):
+        return src
+
+    with open(src['file']) as f:
+        return f.read()
+
+
+def load_secrets(data: dict):
+    data['database']['name'] = load_secret(data['database']['name'])
+    data['database']['user'] = load_secret(data['database']['user'])
+    data['database']['pass'] = load_secret(data['database']['pass'])
+    data['telegram']['token'] = load_secret(data['telegram']['token'])
 
 
 def load(files: list[str]):
     for file in files:
         try:
             with open(file) as f:
-                return json.load(f)
+                data = json.load(f)
+                load_secrets(data)
+                return data
         except FileNotFoundError:
             pass
 
