a43a8160218bc314a03f6391a3ed27b05a910bce
Vyacheslav Syrovatsky <slasyz@gmail.com>
2022-06-29 21:57:29 +0300

    Add asyncpg.

--- toxic/db.py
+++ toxic/db.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-from typing import AsyncIterator
+from typing import AsyncIterator, Iterator, Any
 
+import asyncpg
 import psycopg2
 
 
 class Database:
-    def __init__(self, conn: psycopg2._psycopg.connection):
+    def __init__(self, conn: psycopg2._psycopg.connection, conn_async: asyncpg.Connection):
         self.conn = conn
+        self.conn_async = conn_async
 
     @staticmethod
     async def connect(host: str, port: int, database: str, user: str, password: str) -> Database:
@@ -18,13 +20,23 @@ class Database:
             user=user,
             password=password
         )
+        conn_async = await asyncpg.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
 
-        return Database(conn)
+        return Database(conn, conn_async)
 
     async def exec(self, query, vars=None):
         with self.conn, self.conn.cursor() as cur:
             cur.execute(query, vars)
             self.conn.commit()
+
+    async def exec_async(self, query, vars=None):
+        await self.conn_async.execute(query, *vars)
 
     async def query(self, query, vars=None) -> AsyncIterator:
         with self.conn, self.conn.cursor() as cur:
@@ -38,8 +50,14 @@ class Database:
             for record in cur:
                 yield record
 
+    async def query_async(self, query, vars=None) -> Iterator:
+        return await self.conn_async.fetch(query, *vars)
+
     async def query_row(self, query, vars=None):
         async for record in self.query(query, vars):
             return record
 
         return None
+
+    async def query_row_async(self, query, vars=None) -> Any:
+        return await self.conn_async.fetchrow(query, *vars)
